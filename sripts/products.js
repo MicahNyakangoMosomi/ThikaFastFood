912336a8886c8ddc0e418e6092cc87601b7b2fde
DevMic <nyakangomicah4@gmail.com>
2024-10-29 23:14:44 +0300

    created menu.js and used js to generate html on the homepage

---
+++ sripts/products.js
@@ -0,0 +1,35 @@
+
+export const products =
+[
+    {
+        id:1,
+        discription:'Juicy cheeseburgers with pickled jalapeños topped with a crispy hash brown ',
+        url:'images/burger2.png',
+        price:697
+        
+    
+    },
+    {
+        id:2,
+        discription:'Burritos Wrapped with Beef and Vegetable',
+        url:'images/burritos.png',
+        price:756
+    
+    },
+    {
+        id:3,
+        discription:'Roasted Chicken with Vegetables',
+        url:'images/Roasted Chicken with Vegetables.png',
+        price:1207
+    
+    },
+    {
+        id:4,
+        discription:'Delicious fried chicken with french fries',
+        url:'images/fried chicken.png',
+        price:846
+    
+    } 
+    
+]
+    
