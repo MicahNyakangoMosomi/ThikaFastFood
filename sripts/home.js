7a3700efb8fe86c5c34e87f49b9ab444ea3596a6
DevMic <nyakangomicah4@gmail.com>
2024-11-05 18:11:46 +0300

    made the like hearts and unlike hearts dynamic, added a click event on them

---
+++ sripts/home.js
@@ -0,0 +1,16 @@
+const likeElements = document.querySelectorAll('.like-unlike-image');
+
+likeElements.forEach((element) => {
+    element.addEventListener('click', () => {
+        function like_unlike() {
+
+            if (element.getAttribute("src") === 'images/likeAndUnlike/like.png') {
+                element.setAttribute('src', 'images/likeAndUnlike/unlike.png');
+            } else {
+                element.setAttribute('src', 'images/likeAndUnlike/like.png');
+            }
+        }
+
+        like_unlike();
+    });
+});
