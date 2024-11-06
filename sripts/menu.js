f640884aa0c8621ff92a828219a2ecce7a29d267
DevMic <nyakangomicah4@gmail.com>
2024-11-06 23:44:38 +0300

    done

--- sripts/menu.js
+++ sripts/menu.js
@@ -3,28 +3,32 @@ import {products} from "./products.js";
 const dishes = document.querySelector('.dishes');
 let displayElement='';
 
-products.forEach((product) => {
+function uncategorizedFunction(){
+    products.forEach((product) => {
 
-    displayElement +=`
-    <div class="dish-container product${product.id}">
-        <div class="dish-image"><img src="${product.url}" alt=""></div>
-
-        <div class="dish-content">
-            <p class="dish-text">${product.discription}</p>
-            <div class="like-unlike"><img class="like-unlike-image" src="images/likeAndUnlike/unlike.png" alt=""></div>
+        displayElement +=`
+        <div class="dish-container product${product.id}">
+            <div class="dish-image"><img src="${product.url}" alt=""></div>
+    
+            <div class="dish-content">
+                <p class="dish-text">${product.discription}</p>
+                <div class="like-unlike"><img class="like-unlike-image" src="images/likeAndUnlike/unlike.png" alt=""></div>
+            </div>
+    
+            <div class="price-details">
+                <p class="dish-price">KSH.${product.price}</p>
+                <button class="buy-btn"><a href="">BUY</a></button>
+            </div>
+    
         </div>
+        `
+        dishes.innerHTML= displayElement;   
+    })
 
-        <div class="price-details">
-            <p class="dish-price">KSH.${product.price}</p>
-            <button class="buy-btn"><a href="">BUY</a></button>
-        </div>
+}
+uncategorizedFunction();
 
-    </div>
-    `
-   
 
-    dishes.innerHTML= displayElement;   
-})
 
 const likeElements = document.querySelectorAll('.like-unlike-image');
 
@@ -48,52 +52,47 @@ const sidebar = document.querySelector('.side-bar-list');
 let select = ''
 
 sidebar.addEventListener('click', (event)=>{
+    //Used event Delegation
     const target = event.target
-    if(target.matches('li')){
-        select = target.innerText;
+    
+    if(target.matches('a')){     //this excutes if the target in the ul has an <a> tag
+        select = target.innerText; //declares select which will be the text inside the a tags
         console.log(select.toLowerCase());
-        target.style.backgroundColor="lightgrey";
+        target.style.color="green";
 
         const pageProduct = [];
         products.forEach((product) =>{
         
             if (product.category === select.toLowerCase()){
-                pageProduct.push(product);   
+                pageProduct.push(product); 
+                displayElement = ''
+
+                pageProduct.forEach((product) =>{
+                    displayElement +=`
+                    <div class="dish-container product${product.id}">
+                        <div class="dish-image"><img src="${product.url}" alt=""></div>
+
+                        <div class="dish-content">
+                            <p class="dish-text">${product.discription}</p>
+                            <div class="like-unlike"><img class="like-unlike-image" src="images/likeAndUnlike/unlike.png" alt=""></div>
+                        </div>
+
+                        <div class="price-details">
+                            <p class="dish-price">KSH.${product.price}</p>
+                            <button class="buy-btn"><a href="">BUY</a></button>
+                        </div>
+
+                    </div>
+                    `
+                })
+                dishes.innerHTML= displayElement;  
             }
-            console.log(pageProduct)
-            displayElement = ''
-
-            pageProduct.forEach((product) =>{
-                displayElement +=`
-                <div class="dish-container product${product.id}">
-                    <div class="dish-image"><img src="${product.url}" alt=""></div>
-
-                    <div class="dish-content">
-                        <p class="dish-text">${product.discription}</p>
-                        <div class="like-unlike"><img class="like-unlike-image" src="images/likeAndUnlike/unlike.png" alt=""></div>
-                    </div>
-
-                    <div class="price-details">
-                        <p class="dish-price">KSH.${product.price}</p>
-                        <button class="buy-btn"><a href="">BUY</a></button>
-                    </div>
-
-                </div>
-                `
-
-            })
-
             
-                
-    
-            dishes.innerHTML= displayElement;
-            
-
-        
-            
+          
         })
 
     }
+
 
     
 
