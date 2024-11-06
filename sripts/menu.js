c9f5dc6ea9b768e374e88260608a62e00fe11870
DevMic <nyakangomicah4@gmail.com>
2024-11-06 22:59:12 +0300

    sidebars in menu are now resposive based on the food category

--- sripts/menu.js
+++ sripts/menu.js
@@ -52,40 +52,53 @@ sidebar.addEventListener('click', (event)=>{
     if(target.matches('li')){
         select = target.innerText;
         console.log(select.toLowerCase());
-        target.style.backgroundColor="lightgrey"
-    }
+        target.style.backgroundColor="lightgrey";
+
+        const pageProduct = [];
+        products.forEach((product) =>{
+        
+            if (product.category === select.toLowerCase()){
+                pageProduct.push(product);   
+            }
+            console.log(pageProduct)
+            displayElement = ''
+
+            pageProduct.forEach((product) =>{
+                displayElement +=`
+                <div class="dish-container product${product.id}">
+                    <div class="dish-image"><img src="${product.url}" alt=""></div>
+
+                    <div class="dish-content">
+                        <p class="dish-text">${product.discription}</p>
+                        <div class="like-unlike"><img class="like-unlike-image" src="images/likeAndUnlike/unlike.png" alt=""></div>
+                    </div>
+
+                    <div class="price-details">
+                        <p class="dish-price">KSH.${product.price}</p>
+                        <button class="buy-btn"><a href="">BUY</a></button>
+                    </div>
+
+                </div>
+                `
+
+            })
+
+            
+                
     
-    const pageProduct = [];
-    products.forEach((product) =>{
-       
-        if (product.category === select.toLowerCase()){
-            pageProduct.push(product);   
-        }
-        displayElement = ''
+            dishes.innerHTML= displayElement;
+            
 
-        displayElement +=`
-            <div class="dish-container product${pageProduct.id}">
-                <div class="dish-image"><img src="${pageProduct.url}" alt=""></div>
+        
+            
+        })
 
-                <div class="dish-content">
-                    <p class="dish-text">${pageProduct.discription}</p>
-                    <div class="like-unlike"><img class="like-unlike-image" src="images/likeAndUnlike/unlike.png" alt=""></div>
-                </div>
+    }
 
-                <div class="price-details">
-                    <p class="dish-price">KSH.${pageProduct.price}</p>
-                    <button class="buy-btn"><a href="">BUY</a></button>
-                </div>
+    
 
-            </div>
-            `
-
-       
-          
-    })
-    console.log (pageProduct);
-    dishes.innerHTML= displayElement;
-
+   
+    
     
 
 });
