fe64c9d36ad0fcf3e1cc0d576542833eb3a53ffd
DevMic <nyakangomicah4@gmail.com>
2024-11-06 21:18:52 +0300

    added a variable categoy to the project object

--- sripts/products.js
+++ sripts/products.js
@@ -5,7 +5,8 @@ export const products =
         id:1,
         discription:'Juicy cheeseburgers with pickled jalapeños topped with a crispy hash brown ',
         url:'images/burger2.png',
-        price:697
+        price:697,
+        category:'burgers'
         
     
     },
@@ -13,28 +14,32 @@ export const products =
         id:2,
         discription:'Burritos Wrapped with Beef and Vegetable',
         url:'images/burritos.png',
-        price:756
+        price:756,
+        category:'burritos'
     
     },
     {
         id:3,
         discription:'Roasted Chicken with Vegetables',
         url:'images/Roasted Chicken with Vegetables.png',
-        price:1207
+        price:1207,
+        category:'fries'
     
     },
     {
         id:4,
         discription:'Delicious fried chicken with french fries',
         url:'images/fried chicken.png',
-        price:846
+        price:846,
+        category:'fries'
     
     },
     {
         id:1,
         discription:'Juicy cheeseburgers with pickled jalapeños topped with a crispy hash brown ',
         url:'images/burger2.png',
-        price:697
+        price:697,
+        category:'burgers'
         
     
     },
@@ -42,28 +47,32 @@ export const products =
         id:2,
         discription:'Burritos Wrapped with Beef and Vegetable',
         url:'images/burritos.png',
-        price:756
+        price:756,
+        category:'burritos'
     
     },
     {
         id:3,
         discription:'Roasted Chicken with Vegetables',
         url:'images/Roasted Chicken with Vegetables.png',
-        price:1207
+        price:1207,
+        category:'fries'
     
     },
     {
         id:4,
         discription:'Delicious fried chicken with french fries',
         url:'images/fried chicken.png',
-        price:846
+        price:846,
+        category:'fries'
     
     },
     {
         id:1,
         discription:'Juicy cheeseburgers with pickled jalapeños topped with a crispy hash brown ',
         url:'images/burger2.png',
-        price:697
+        price:697,
+        category:'burgers'
         
     
     },
@@ -71,28 +80,32 @@ export const products =
         id:2,
         discription:'Burritos Wrapped with Beef and Vegetable',
         url:'images/burritos.png',
-        price:756
+        price:756,
+        category:'burritos'
     
     },
     {
         id:3,
         discription:'Roasted Chicken with Vegetables',
         url:'images/Roasted Chicken with Vegetables.png',
-        price:1207
+        price:1207,
+        category:'fries'
     
     },
     {
         id:4,
         discription:'Delicious fried chicken with french fries',
         url:'images/fried chicken.png',
-        price:846
+        price:846,
+        category:'fries'
     
     },
     {
         id:1,
         discription:'Juicy cheeseburgers with pickled jalapeños topped with a crispy hash brown ',
         url:'images/burger2.png',
-        price:697
+        price:697,
+        category:'burgers'
         
     
     },
@@ -100,28 +113,32 @@ export const products =
         id:2,
         discription:'Burritos Wrapped with Beef and Vegetable',
         url:'images/burritos.png',
-        price:756
+        price:756,
+        category:'burritos'
     
     },
     {
         id:3,
         discription:'Roasted Chicken with Vegetables',
         url:'images/Roasted Chicken with Vegetables.png',
-        price:1207
+        price:1207,
+        category:'fries'
     
     },
     {
         id:4,
         discription:'Delicious fried chicken with french fries',
         url:'images/fried chicken.png',
-        price:846
+        price:846,
+        category:'fries'
     
     },
     {
         id:1,
         discription:'Juicy cheeseburgers with pickled jalapeños topped with a crispy hash brown ',
         url:'images/burger2.png',
-        price:697
+        price:697,
+        category:'burgers'
         
     
     },
@@ -129,82 +146,25 @@ export const products =
         id:2,
         discription:'Burritos Wrapped with Beef and Vegetable',
         url:'images/burritos.png',
-        price:756
+        price:756,
+        category:'burritos'
     
     },
     {
         id:3,
         discription:'Roasted Chicken with Vegetables',
         url:'images/Roasted Chicken with Vegetables.png',
-        price:1207
+        price:1207,
+        category:'fries'
     
     },
     {
         id:4,
         discription:'Delicious fried chicken with french fries',
         url:'images/fried chicken.png',
-        price:846
+        price:846,
+        category:'fries'
     
-    },
-    {
-        id:5,
-        discription:'Juicy cheeseburgers with pickled jalapeños topped with a crispy hash brown ',
-        url:'images/burger2.png',
-        price:697
-        
-    
-    },
-    {
-        id:6,
-        discription:'Burritos Wrapped with Beef and Vegetable',
-        url:'images/burritos.png',
-        price:756
-    
-    },
-    {
-        id:7,
-        discription:'Roasted Chicken with Vegetables',
-        url:'images/Roasted Chicken with Vegetables.png',
-        price:1207
-    
-    },
-    {
-        id:8,
-        discription:'Delicious fried chicken with french fries',
-        url:'images/fried chicken.png',
-        price:846
-    
-    },
-    {
-        id:9,
-        discription:'Juicy cheeseburgers with pickled jalapeños topped with a crispy hash brown ',
-        url:'images/burger2.png',
-        price:697
-        
-    
-    },
-    {
-        id:10,
-        discription:'Burritos Wrapped with Beef and Vegetable',
-        url:'images/burritos.png',
-        price:756
-    
-    },
-    {
-        id:11,
-        discription:'Roasted Chicken with Vegetables',
-        url:'images/Roasted Chicken with Vegetables.png',
-        price:1207
-    
-    },
-    {
-        id:12,
-        discription:'Delicious fried chicken with french fries',
-        url:'images/fried chicken.png',
-        price:846
-    
-    },
-    
-    
+    }
+
 ]
-    
